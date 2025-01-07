1ea3bde4f8bbbf3e4c2c6607b1c86fbb57ded9a6
Max Korbmacher <64847184+MaxKorbmacher@users.noreply.github.com>
2025-01-07 09:38:13 +0100

    minor update

--- BL_analysis.R
+++ BL_analysis.R
@@ -1,96 +1,117 @@
 # Baseline differences: functionally stable group (FSG) vs functional loss group (FLG)
 # Max Korbmacher, 03.12.2024
 #
+rm(list = ls(all.names = TRUE)) # clear all objects includes hidden objects.
+gc() #free up memory and report the memory usage.
 # define data path
 datapath = "/Users/max/Documents/Local/MS/results/"
 # read data
 data = read.csv(paste(datapath,"interrim_data.csv",sep=""))
 # pkgs
 library(dplyr)
+library(datawizard)
+library(rlist)
+library(WebPower)
 #
-# BL differemces between FLG and non-FLG patients ####
-# Differences in continous scores ####
+data = data_unique(data = data, select = eid)
 
-# age
-FLG.imp = lm(age ~ FLG + sex, data)
-summary(FLG.imp)
-
-# Mean_BMI_OFAMS
-FLG.imp = lm(Mean_BMI_OFAMS ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# EDSS
-FLG.imp = lm(edss ~ FLG + age + sex,data)
-summary(FLG.imp)
-data %>% group_by(FLG) %>% summarize(M=mean(edss),SD=sd(edss))
-
-# PASAT
-FLG.imp = lm(PASAT ~ FLG + age + sex,data)
-summary(FLG.imp)
-
-# relapses_12mnths_before_baseline
-FLG.imp = lm(relapses_12mnths_before_baseline ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# CH3L.1..mg.ml..mean
-FLG.imp = lm(CH3L.1..mg.ml..mean ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# NfL..pg.ml.
-FLG.imp = lm(NfL..pg.ml. ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# BAG_c
-FLG.imp = lm(BAG_c ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# baselineC
-FLG.imp = lm(baselineC ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# baselineV
-FLG.imp = lm(baselineV ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# PF
-FLG.imp = lm(PF ~ FLG + age + sex, data)
-summary(FLG.imp)
-max(na.omit(data$PF))
-# RF
-FLG.imp = lm(RF ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# BP
-FLG.imp = lm(BP ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# GH
-FLG.imp = lm(GH ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# VT
-FLG.imp = lm(VT ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# SF
-FLG.imp = lm(SF ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# RE
-FLG.imp = lm(RE ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-# MH                              
-FLG.imp = lm(MH ~ FLG + age + sex, data)
-summary(FLG.imp)
-
-
-
+# 1. POWER ####
+# power is calculated from another script (power.R or power_optimized.R for parallelisation)
+# we use some descriptives on power and model fit
+pwr = read.csv("/Users/max/Documents/Local/MS/results/model_info.csv")
+# all models
+hist(pwr$Power)
+hist(pwr$McFaddenR2)
+median(na.omit(pwr$Power))
+mad(na.omit(pwr$Power))
+median(na.omit(pwr$McFaddenR2))
+mad(na.omit(pwr$McFaddenR2))
+nrow(pwr) # number of all models ran
+sum(is.na(pwr$Power)) # models with convergence errors disallowing power calculation
+# high enough power models
+pwr = pwr %>% filter(Power >= .8)
+hist(pwr$Power)
+hist(pwr$McFaddenR2)
+median(na.omit(pwr$Power))
+mad(na.omit(pwr$Power))
+median(na.omit(pwr$McFaddenR2))
+mad(na.omit(pwr$McFaddenR2))
+nrow(pwr) # number of all models ran
+# high enough power and excellent model fit
+pwr = pwr %>% filter(McFaddenR2 >= .2)
+hist(pwr$Power)
+hist(pwr$McFaddenR2)
+median(na.omit(pwr$Power))
+mad(na.omit(pwr$Power))
+median(na.omit(pwr$McFaddenR2))
+mad(na.omit(pwr$McFaddenR2))
+nrow(pwr) # number of all models ran
+rm(pwr)
+#
+#
+#
+# 2. DEMOGRAPHICS: BL differences between FLG and non-FLG patients ####
+# Differences in continuous scores ####
+demotab=function(var){
+  M_FLG = mean(unlist(na.omit(data%>%filter(FLG == 1))[var]))
+  SD_FLG = sd(unlist(na.omit(data%>%filter(FLG == 1))[var]))
+  M_FSG = mean(unlist(na.omit(data%>%filter(FLG == 0))[var]))
+  SD_FSG = sd(unlist(na.omit(data%>%filter(FLG == 0))[var]))
+  FLG.imp = lm(formula(paste(var,"~ FLG + sex")), data)
+  B = summary(FLG.imp)$coefficients[2]
+  SE = summary(FLG.imp)$coefficients[2,2]
+  CI_low = B-1.96*SE
+  CI_high = B+1.96*SE
+  t = summary(FLG.imp)$coefficients[2,3]
+  p = summary(FLG.imp)$coefficients[2,4]
+  return(data.frame(var,M_FLG,SD_FLG,M_FSG,SD_FSG,B,SE,CI_low, CI_high, t,p))
+}
+contvar = c("BAG_c","baselineC","baselineV","edss","PASAT",
+            "relapses_12mnths_before_baseline","age","BL_BMI",
+            "CH3L.1..mg.ml..mean", "NfL..pg.ml.",
+            "Vit_A_0", "Vit_D_0","Vit_E_0", "BP", "GH", "MH",
+            "PF", "RE", "RF", "SF", "VT")
+demg = list()
+for (i in 1:length(contvar)){
+  demg[[i]] = demotab(contvar[i])
+}
+demg = list.rbind(demg)
+demg[2:(ncol(demg)-1)] = round(demg[2:(ncol(demg)-1)], 2)
+demg$p = round(demg$p,4)
+demg$FSG = paste(demg$M_FSG," (",demg$SD_FSG,")",sep="")
+demg$FLG = paste(demg$M_FLG," (",demg$SD_FLG,")",sep="")
+demg$Difference = paste(demg$B," (",demg$SE,")",sep="")
+demg = demg %>% dplyr::select(FLG,FSG,Difference,CI_low,CI_high,t,p)
+write.csv(x = demg, paste(datapath,"descriptive_continuous.csv",sep=""))
+#
+#
+#
 # Differences in frequencies ####
-chisq.test((data$geno),data$FLG,simulate.p.value = TRUE, B = 1000000) # genotype diff
-chisq.test((data$sex),data$FLG,simulate.p.value = TRUE, B = 1000000) # sex diff
-chisq.test((data$smoking_OFAMS),data$FLG,simulate.p.value = TRUE, B = 1000000)
-chisq.test((data$Treatment_OFAMS),data$FLG,simulate.p.value = TRUE, B = 1000000)
-chisq.test((data$Omega3_suppl),data$FLG,simulate.p.value = TRUE, B = 1000000)
-
-
+# FLG 1 = FLG
+# sex: 1=female, 0=male
+binaries = c("sex", "smoking_OFAMS","Omega3_suppl", "geno")
+res=list()
+for (i in 1:length(binaries)){
+  # how the sausage is made, but obsolete for OR, CI, p:
+  a = table(data.frame(data["FLG"],data[paste(binaries[i])]))[4] # both 1
+  b = table(data.frame(data["FLG"],data[paste(binaries[i])]))[1] # both 0
+  c = table(data.frame(data["FLG"],data[paste(binaries[i])]))[3] # only FLG 1
+  d = table(data.frame(data["FLG"],data[paste(binaries[i])]))[2] # only var of interest 1
+  # OR = (a * b) / (c * d) # resulting odds ratio
+  # SE = sqrt(1/a + 1/b + 1/c + 1/d)
+  # CI_low = exp(log(OR)-1.96*SE)
+  # CI_high = exp(log(OR)+1.96*SE)
+  # data.frame(OR,SE,CI_low, CI_high)
+  res[[i]] = c(a,b,c,d,(fisher.test(table(data.frame(data["FLG"],data[paste(binaries[i])])))$estimate),
+    unlist(fisher.test(table(data.frame(data["FLG"],data[paste(binaries[i])])))[2]),
+    unlist(fisher.test(table(data.frame(data["FLG"],data[paste(binaries[i])])))[1]))
+}
+res = data.frame(list.rbind(res))
+rownames(res) = binaries
+res$OR=paste(round(res$odds.ratio,2)," [",round(res$conf.int1,2),",",round(res$conf.int2,2),"]",sep="")
+res = res %>% dplyr::select(V1,V2,V3,V4,OR,p.value)
+names(res) = c("FLG & Group membership = yes","FLG & Group membership = no", 
+               "FSG & Group membership = yes","FSG & Group membership = no",
+               "OR","p")
+write.csv(x = res, paste(datapath,"descriptive_binary.csv",sep=""))
+#
---
+++ EDSS_Logit_multiverse_optimised.R
@@ -0,0 +1,55 @@
+# Multiverse for Logistic Regression Models
+# ChatGPT aided optimisation, Max Korbmacher 06 December 2024
+#
+library(dplyr)
+library(pscl)
+library(parallel)
+#
+# Load data
+data <- read.csv("/Users/max/Documents/Local/MS/results/interrim_data.csv")
+#
+#
+# Predictors
+predictors <- c("geno", "relapses_12mnths_before_baseline", "CH3L.1..mg.ml..mean", 
+                "NfL..pg.ml.", "edss", "PASAT", "smoking_OFAMS", "BL_BMI", 
+                "Omega3_suppl", "BAG_c", "baselineC", "baselineV", 
+                "PF", "RF", "BP", "GH", "VT", "SF", "RE", "MH", "Vit_A_0", "Vit_D_0", 
+                "Vit_E_0") # "Treatment_OFAMS", 
+
+# Generate model formulas
+max_order <- 8
+all_combinations <- lapply(1:max_order, function(i) combn(predictors, i, simplify = FALSE))
+all_formulas <- unlist(lapply(all_combinations, function(comb_list) {
+  lapply(comb_list, function(comb) as.formula(paste("FLG ~ age +", paste(comb, collapse = " + "))))
+}))
+# Add optional terms (here, this is the covariate sex)
+all_formulas <- c(all_formulas, lapply(all_formulas, function(f) update(f, . ~ . + sex)))
+#
+# Run models in parallel
+num_cores <- detectCores() - 4
+results <- mclapply(all_formulas, function(formula) {
+  model <- glm(formula, family = "binomial", data = data)
+  coefs <- data.frame(
+    Names = names(model$coefficients),
+    Beta = model$coefficients,
+    SE = summary(model)$coefficients[, 2],
+    P = summary(model)$coefficients[, 4]
+  )
+  list(
+    formula = as.character(formula),
+    pseudoR2 = pscl::pR2(model)["McFadden"],
+    coefs = coefs
+  )
+}, mc.cores = num_cores)
+
+# Combine results
+model_info <- do.call(rbind, lapply(results, function(res) {
+  data.frame(Formula = res$formula, McFaddenR2 = res$pseudoR2)
+}))
+coef_table <- do.call(rbind, lapply(results, function(res) res$coefs))
+
+# Write results
+write.csv(model_info, "/Users/max/Documents/Local/MS/results/EDSS_model_info_optimised.csv", row.names = FALSE)
+write.csv(coef_table, "/Users/max/Documents/Local/MS/results/EDSS_stratification_optimised.csv", row.names = FALSE)
+
+print("Done.")
