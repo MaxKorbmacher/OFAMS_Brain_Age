b6a673d37efd53e81220b731cba316cbcbb42c21
Max Korbmacher <64847184+MaxKorbmacher@users.noreply.github.com>
2025-05-20 13:19:00 +0200

    Add files via upload

--- simulate.R
+++ simulate.R
@@ -2,72 +2,37 @@
 # Max Korbmacher
 # May 2025
 #
-#install.packages("synthpop")
+#
+# clean up
+rm(list = ls(all.names = TRUE)) # clear all objects includes hidden objects.
+gc() #free up memory and report the memory usage.
+#
+# packages
+# install.packages("synthpop")
+# install.packages("regclass")
 library(synthpop)
 library(rlist)
-data <- read.csv("/Users/max/Documents/Local/MS/results/interrim_data.csv")
-data = data %>% select("CLG","Current_DMT","geno", "relapses_12mnths_before_baseline", 
-                                "NfL..pg.ml.", "edss", "PASAT", "smoking_OFAMS", "BL_BMI",
-                                "Omega3_suppl", "BAG_c", "baselineC", "baselineV",
-                                "Vit_A_0","Vit_D_0", "Vit_E_0",
-                                "PF", "BP", "GH", "VT", "SF","MH","sex","age")
-mysyn = syn(data,m=10)
-summary(mysyn)
-data = list.rbind(mysyn$syn)
-
-model = glm(CLG ~ age + Current_DMT + sex + geno+ relapses_12mnths_before_baseline+ 
-                edss+ PASAT+ smoking_OFAMS+
-                Vit_A_0+Vit_D_0+ Vit_E_0,
-                data=data,family = "binomial")
-summary(model)
-
-#
-#
-#
-#
-#
-#
-
-
-data <- read.csv("/Users/max/Documents/Local/MS/results/interrim_data.csv")
-data = data %>% select("FLG","Current_DMT","geno", "relapses_12mnths_before_baseline", 
-                       "NfL..pg.ml.", "edss", "PASAT", "smoking_OFAMS", "BL_BMI",
-                       "Omega3_suppl", "BAG_c", "baselineC", "baselineV",
-                       "Vit_A_0","Vit_D_0", "Vit_E_0",
-                       "PF", "BP", "GH", "VT", "SF","MH","sex","age")
-mysyn = syn(data,m=10)
-summary(mysyn)
-data = list.rbind(mysyn$syn)
-
-model = glm(FLG ~ age + Current_DMT + sex + geno+ relapses_12mnths_before_baseline+ 
-              edss+ PASAT+ smoking_OFAMS+
-              Vit_A_0+Vit_D_0+ Vit_E_0,
-            data=data,family = "binomial")
-summary(model)
-
-
-
-
-
-
-install.packages("finalfit")
-library("finalfit")
 library(dplyr)
-explanatory = c("Current_DMT","geno", "relapses_12mnths_before_baseline", 
-  "NfL..pg.ml.", "edss", "PASAT", "smoking_OFAMS", "BL_BMI",
-  "Omega3_suppl", "BAG_c", "baselineC", "baselineV",
-  "Vit_A_0","Vit_D_0", "Vit_E_0",
-  "PF", "BP", "GH", "VT", "SF","MH","sex","age")
-dependent="CLG"
-
-data %>%
-  finalfit_newdata(explanatory = explanatory, newdata = list(
-    c("<40 years",  "Submucosa", "No"),
-    c("<40 years", "Submucosa", "Yes")
-    )) -> newdata
-
-data %>% glmmulti(dependent, explanatory) %>% 
-  boot_predict(newdata, 
-               estimate_name = "Predicted probability of death",
-               R=100, boot_compare = FALSE,
-               digits = c(2,3))
+library(regclass)
+#
+# data
+data <- read.csv("/Users/max/Documents/Local/MS/results/interrim_data.csv")
+names(data)
+data = data%>%select(-X,-CH3L.1..mg.ml..mean,-SYMPTOM_DURATION,-DISEASE_DURATION, -age_gap,-eid, -Omega3_suppl)
+# data = data %>% select("CLG","Current_DMT","geno", "relapses_12mnths_before_baseline", 
+#                                 "NfL..pg.ml.", "edss", "PASAT", "smoking_OFAMS", "BL_BMI",
+#                                 "Omega3_suppl", "BAG_c", "baselineC", "baselineV",
+#                                 "Vit_A_0","Vit_D_0", "Vit_E_0",
+#                                 "PF", "BP", "GH", "VT", "SF","MH","sex","age")
+# factor specification
+data$FLG = factor(data$FLG)
+data$CLG = factor(data$CLG)
+data$sex = factor(data$sex)
+data$smoking_OFAMS = factor(data$smoking_OFAMS)
+data$Treatment_OFAMS = factor(data$Treatment_OFAMS)
+data$Current_DMT = factor(data$Current_DMT)
+# creating synthetic data
+mysyn = syn(data,m=100,k=10000,seed = 123)
+summary(mysyn)
+data = list.rbind(mysyn$syn)
+write.csv(data, "/Users/max/Documents/Local/MS/results/synthetic_data.csv")
